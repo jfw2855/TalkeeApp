2399fc461095d2422ca286915237f5de122f291f
Justin Waddle <justinfrankwaddle@gmail.com>
2022-03-18 10:03:22 -0500

    post route working for comments

--- controllers/comment.js
+++ controllers/comment.js
@@ -1,125 +1,54 @@
+////////////////////////////////////////////
 // Import Dependencies
+////////////////////////////////////////////
 const express = require('express')
-const Example = require('../models/example')
+const mongoose = require('mongoose')
+const Social = require('../models/social')
+const comment = require('../models/comment')
 
+////////////////////////////////////////////
 // Create router
+////////////////////////////////////////////
 const router = express.Router()
 
-// Router Middleware
-// Authorization middleware
-// If you have some resources that should be accessible to everyone regardless of loggedIn status, this middleware can be moved, commented out, or deleted. 
-router.use((req, res, next) => {
-	// checking the loggedIn boolean of our session
-	if (req.session.loggedIn) {
-		// if they're logged in, go to the next thing(thats the controller)
-		next()
-	} else {
-		// if they're not logged in, send them to the login page
-		res.redirect('/auth/login')
-	}
-})
-
+////////////////////////////////////////////
 // Routes
+////////////////////////////////////////////
+// only need two routes for comments right now
+// POST -> to create a comment
 
-// index ALL
-router.get('/', (req, res) => {
-	Example.find({})
-		.then(examples => {
-			const username = req.session.username
-			const loggedIn = req.session.loggedIn
-			
-			res.render('examples/index', { examples, username, loggedIn })
-		})
-		.catch(error => {
-			res.redirect(`/error?error=${error}`)
-		})
+
+router.post('/add', (req, res) => {
+    console.log('first comment body', req.body)
+    
+    // adjusts req.body to include an author
+    // the author's id will be the logged in user's id
+    req.body.author = req.session.userId
+    console.log('updated comment body', req.body)
+    // find all social contents
+    Social.findOne({title:'message board'})
+        .then(social => {
+            // sends req.body to the comments array
+            console.log("this is the entry", social)
+            social.comments.push(req.body)
+            return social.save()
+
+        })
+        .then(social => {
+            console.log("updated comments section", social.comments)
+            // redirect
+            res.redirect(`/social`)
+        })
+        // or show an error if we have one
+        .catch(error => {
+            console.log(error)
+            res.send(error)
+        })
 })
 
-// index that shows only the user's examples
-router.get('/mine', (req, res) => {
-    // destructure user info from req.session
-    const { username, userId, loggedIn } = req.session
-	Example.find({ owner: userId })
-		.then(examples => {
-			res.render('examples/index', { examples, username, loggedIn })
-		})
-		.catch(error => {
-			res.redirect(`/error?error=${error}`)
-		})
-})
 
-// new route -> GET route that renders our page with the form
-router.get('/new', (req, res) => {
-	const { username, userId, loggedIn } = req.session
-	res.render('examples/new', { username, loggedIn })
-})
 
-// create -> POST route that actually calls the db and makes a new document
-router.post('/', (req, res) => {
-	req.body.ready = req.body.ready === 'on' ? true : false
-
-	req.body.owner = req.session.userId
-	Example.create(req.body)
-		.then(example => {
-			console.log('this was returned from create', example)
-			res.redirect('/examples')
-		})
-		.catch(error => {
-			res.redirect(`/error?error=${error}`)
-		})
-})
-
-// edit route -> GET that takes us to the edit form view
-router.get('/:id/edit', (req, res) => {
-	// we need to get the id
-	const exampleId = req.params.id
-	Example.findById(exampleId)
-		.then(example => {
-			res.render('examples/edit', { example })
-		})
-		.catch((error) => {
-			res.redirect(`/error?error=${error}`)
-		})
-})
-
-// update route
-router.put('/:id', (req, res) => {
-	const exampleId = req.params.id
-	req.body.ready = req.body.ready === 'on' ? true : false
-
-	Example.findByIdAndUpdate(exampleId, req.body, { new: true })
-		.then(example => {
-			res.redirect(`/examples/${example.id}`)
-		})
-		.catch((error) => {
-			res.redirect(`/error?error=${error}`)
-		})
-})
-
-// show route
-router.get('/:id', (req, res) => {
-	const exampleId = req.params.id
-	Example.findById(exampleId)
-		.then(example => {
-            const {username, loggedIn, userId} = req.session
-			res.render('examples/show', { example, username, loggedIn, userId })
-		})
-		.catch((error) => {
-			res.redirect(`/error?error=${error}`)
-		})
-})
-
-// delete route
-router.delete('/:id', (req, res) => {
-	const exampleId = req.params.id
-	Example.findByIdAndRemove(exampleId)
-		.then(example => {
-			res.redirect('/examples')
-		})
-		.catch(error => {
-			res.redirect(`/error?error=${error}`)
-		})
-})
-
+////////////////////////////////////////////
 // Export the Router
+////////////////////////////////////////////
 module.exports = router
