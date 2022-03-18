f263cd62b34f3683a43c5f379f4e14810b66d3c9
Justin Waddle <justinfrankwaddle@gmail.com>
2022-03-18 09:57:26 -0500

    adds a title field

--- models/comment.js
+++ models/comment.js
@@ -1,27 +1,18 @@
 // import dependencies
 const mongoose = require('./connection')
 
-// import user model for populate
-const User = require('./user')
-
 // destructure the schema and model constructors from mongoose
-const { Schema, model } = mongoose
 
-const commentSchema = new Schema(
-	{
+const commentSchema = new mongoose.Schema({
 		note: { type: String, required: true },
-		posted: new Date,
 		author: {
-			type: Schema.Types.ObjectId,
-			ref: 'User'
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'User',
+			required: true
 		}
 	},
 	{ timestamps: true }
 )
 
-const Comment = model('Comment', commentSchema)
 
-/////////////////////////////////
-// Export our Model
-/////////////////////////////////
-module.exports = Comment
+module.exports = commentSchema
