5cdd7fb3d93382b5d1a79e15a0077a30e0ec2673
Justin Waddle <justinfrankwaddle@gmail.com>
2022-03-18 15:51:06 -0500

    delete route working

--- controllers/comment.js
+++ controllers/comment.js
@@ -46,7 +46,59 @@ router.post('/add', (req, res) => {
         })
 })
 
+// DELETE -> to destroy a comment
 
+//we'll use two params to make our life easier
+//first the id of the fruit, since we need to find it
+//then the id of the comment, since w want to delete it!!!!
+router.delete('/', async (req,res)=> {
+    //first we want to parse out our id
+    const commId = req.body.id
+    console.log("commIdddddd",{commId})
+    // then we'll find the fruit
+    Social.find({})
+        .then(social => {
+            console.log('social',social)
+
+            let x = social[0].comments.id(commId)
+            console.log('delete: THIS IS X ', x)
+            const theComment = commId
+            x.remove()
+            return social[0].save()
+
+
+
+            // for (let i = 0; i<x.length;i++ ) {
+            //     if(x[i]._id==` new ObjectId("6231ef05f06e5a9a6308a1b1")`){
+            //         console.log('found @',x[i]._id)
+            //     }
+            //     else {console.log('not founnd @',x[i]._id)}
+
+            // }
+            // social[0].update({title:"message board"}, {$pull: {comments: {_id: theComment}}})
+                // return social[0].save()
+            // console.log("this is the first comment's author",social.comments[0].author)
+            //     // only delete the comment if the user who is logged in is the comment's author
+            //     if (theComment.author == req.session.userId) {
+            //     // then we'll delete the comment
+            //     theComment.remove()
+            //     // return the saved social
+            //     return social.save()
+            //     }else {
+            //         return
+            //     }
+                
+        })
+    .then (() => { 
+        // redirects back to social page
+        res.redirect(`/social`)
+    })
+       // catch any errors!
+        .catch(error => {
+            console.log(error)
+            res.send(error)
+        })
+})
 
 ////////////////////////////////////////////
 // Export the Router
